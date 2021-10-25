07a95ba2cc16b34b810cb4cc8909aecab8a91599
mavi2011 <e4.mavi@gmail.com>
2021-10-25 15:57:08 +0530

    save all

--- p5.js
+++ p5.js
@@ -1,20 +1,38 @@
+faceX=0;
+faceY=0;
 function preload(){
-
+mustache_on_face=loadImage('https://i.postimg.cc/zBSNZQcy/unnamed.png');
 }
 
 function setup(){
     canvas=createCanvas(300, 300);
     canvas.center();
-    video=creatcapture(VIDEO);
-    video.size(300, 300);
-    video.hide();
+   video=createCapture(VIDEO);
+   video.size(300, 300);
+   video.hide();
+   poseNet=ml5. poseNet(video. modelLoaded); 
+   poseNet.on('pose', gotPoses); 
 }
 
 function draw(){
-fill('red')
-ellipse(mouseX, mouseY, 40, 40);
+    image(video, 0, 0, 300, 300);
+    image(mustache_on_face, faceX, faceY, 80,35);
 }
 
 function take_snapshot(){
     save('myFilterImage.png'); 
+}
+
+function modelLoaded()
+{ 
+    console.log("modelReady");
+ }
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        faceX=results[0].pose.face.x -40;
+        faceY=results[0].pose.face.y;
+        console.log("face X = "+faceX);
+        console.log("face Y = "+ faceY);
+    }
 }
